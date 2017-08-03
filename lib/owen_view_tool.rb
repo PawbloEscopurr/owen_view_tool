67d0fa5357df93d14e61cd4570b6d27efbb97fa5
Jordan Hudgens <jordan@devcamp.com>
2017-08-03 15:24:49 +0000

    Dev tool intitial commit

---
+++ lib/owen_view_tool.rb
@@ -0,0 +1,6 @@
+require "owen_view_tool/version"
+require "owen_view_tool/renderer"
+
+module OwenViewTool
+  # Your code goes here...
+end
---
+++ lib/owen_view_tool/version.rb
@@ -0,0 +1,3 @@
+module OwenViewTool
+  VERSION = "0.1.0"
+end
